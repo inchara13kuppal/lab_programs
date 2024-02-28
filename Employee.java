d3f6b0ec31ef2dd284a26419086d2509cdd9f6bd
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2024-02-28 21:15:13 +0530

    week2_p11

---
+++ Employee.java
@@ -0,0 +1,34 @@
+package week2;
+
+class Employee {
+    String empName;
+    int empNo;
+    float salary;
+    String empDesig;
+    byte expYrs;
+
+    void setEmployee(String name, int No, float Sal, String Des, byte Ex) {
+        empName = name;
+        empNo = No;
+        salary = Sal;
+        empDesig = Des;
+        expYrs = Ex;
+    }
+
+    void getEmployee() {
+        System.out.println(empName + "\t" + empNo + "\t" + salary + "\t" + empDesig + "\t" + expYrs + "\n");
+    }
+}
+
+class DemoEmployee {
+    public static void main(String args[]) {
+        Employee rahul = new Employee();
+        rahul.setEmployee("Rahul", 128966, 99999.0f, "Project Manager",(byte) 20);
+        System.out.println("empName\tempNo\tsalary\tempDesig\texpYrs\n");
+        rahul.getEmployee();
+        
+        Employee rakesh = new Employee();
+        rakesh.setEmployee("Rakesh", 128969, 98765.4f, "Project lead", (byte) 10);
+        rakesh.getEmployee();
+    }
+}
