e2e006e73e4fab906c39ffa32b57d52f2e35648f
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2024-03-03 03:04:53 +0530

    week5_p7

---
+++ SequentialSearch.java
@@ -0,0 +1,36 @@
+package week5;
+
+import java.util.Scanner;
+
+public class SequentialSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the element to search: ");
+        int element = sc.nextInt();
+
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == element) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+
+        sc.close();
+    }
+}
