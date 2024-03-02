0d6dd620ed1cf2b597b07d4d4cbe34718543c2a5
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2024-03-03 02:39:53 +0530

    week5_p4

---
+++ SumofNegativeNo.java
@@ -0,0 +1,28 @@
+package week5;
+
+import java.util.Scanner;
+
+class SumOfNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                sum += arr[i];
+            }
+        }
+        
+        System.out.println("The sum of negative numbers = " + sum);
+        sc.close();
+    }
+}
+
