ee7c90f6ec385b77b2f2ceff9cdd55c32b9cfac0
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2024-03-14 16:00:18 +0530

    week1

---
+++ PositiveNegative.java
@@ -0,0 +1,10 @@
+class PositiveNegative {
+    public static void main(String[] args) {
+        int num = -10;
+        if (num > 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is negative");
+        }
+    }
+}
