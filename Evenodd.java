0eafc6d1f7806c183abc09aa16014516f2d6957b
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2024-03-14 15:53:03 +0530

    week1

---
+++ Evenodd.java
@@ -0,0 +1,33 @@
+public class Evenodd
+
+{
+
+	public static void main(String args[])
+
+	{
+
+	System.out.println("This program checks the number is odd or even");
+
+
+
+	int num=68;
+
+	if(num%2==0)
+
+		System.out.println("The given number "+num+" is Even");
+
+	else
+
+
+
+	System.out.println("The given number "+num+" is Odd");
+
+
+
+	}
+
+
+
+
+
+}
